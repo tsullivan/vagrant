13ba39647dad577ccf929ce5313b8f86b978aaae
Timothy Sullivan <tsullivan@elastic.co>
2019-01-18 13:30:19 -0700

    JAVA_HOME for oracle

--- share/scripts/oraclelinux-6.10.sh
+++ share/scripts/oraclelinux-6.10.sh
@@ -4,3 +4,8 @@ set -e
 set -o pipefail
 
 yum install -y tar wget java
+
+echo "Setting up environment..."
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.191.b12-0.el6_10.x86_64/jre
+export JAVABIN=$JAVA_HOME/bin
+export PATH=$PATH:$JAVABIN
