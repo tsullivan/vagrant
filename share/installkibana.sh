de3b7057c3c1a7c82e8d9a52c89330741fae783a
Timothy Sullivan <tsullivan@elastic.co>
2021-11-04 13:50:30 -0700

    update kibana download

--- share/installkibana.sh
+++ share/installkibana.sh
@@ -1,5 +1,5 @@
 cd $HOME
 
-wget https://snapshots.elastic.co/8.1.0-2fdb91c4/downloads/kibana/kibana-8.1.0-SNAPSHOT-linux-x86_64.tar.gz
+wget https://snapshots.elastic.co/8.1.0-f479a40a/downloads/kibana/kibana-8.1.0-SNAPSHOT-linux-x86_64.tar.gz
 sudo apt install -y libnss3 fonts-liberation libfontconfig1
 alias kibana="/home/vagrant/kibana-8.1.0-SNAPSHOT-linux-x86_64/bin/kibana"
