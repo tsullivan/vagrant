efec03002a603fb4f10a3cc18b95c75ad14d5491
Timothy Sullivan <tsullivan@elastic.co>
2021-11-09 12:40:24 -0700

    centos

--- Vagrantfile
+++ Vagrantfile
@@ -2,18 +2,43 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/focal64"
   config.vm.network :forwarded_port, guest: 5601, host: 5666 # Start Kibana and go to localhost:5666 in your host's browser
+
   config.vm.provider "virtualbox" do |vbox|
+    vbox.customize ["modifyvm", :id, "--audio", "none"]
     vbox.memory = 1024
-    vbox.cpus = 4
+    vbox.cpus = 2
     vbox.gui = false
   end
 
-  # https://islascruz.org/blog/2017/08/30/quick-tip-vagrant-ram/
-  # enable auto-sizing swap service
-  config.vm.provision "shell", inline: "sudo apt-get install swapspace -y"
+  config.vm.define "kibana-reporting-centos" do |machine|
+    config.vm.box = "centos/8"
+    config.vm.box_version = "2011.0"
+
+    # Set up Centos
+    config.vm.provision "shell", inline: <<-SHELL
+      dnf update -y
+      dnf install libnss3.so xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc fontconfig freetype -y
+      dnf install wget jq -y
+      SHELL
+    end
+
+  config.vm.define "kibana-reporting-ubuntu" do |machine|
+    config.vm.box = "ubuntu/focal64"
+    config.vm.box_version = "20211026.0.0"
+
+    # https://islascruz.org/blog/2017/08/30/quick-tip-vagrant-ram/
+    # enable auto-sizing swap service
+    config.vm.provision "shell", inline: "sudo apt-get install swapspace -y"
+
+    # Set up Ubuntu
+    config.vm.provision "shell", inline: <<-SHELL
+      apt-get update
+      apt-get install -y libnss3 fonts-liberation libfontconfig1
+      apt-get install -y jq
+      SHELL
+    end
 
   # Set up
-  config.vm.provision "shell", inline: "sh /vagrant/share/bootstrap.sh"
+  config.vm.provision "shell", inline: "sh /vagrant/bootstrap.sh"
 end
---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+set -e
+VERSION=7.16.0 # version of snapshot builds to download
+
+MANIFEST=$(curl --silent -XGET https://artifacts-api.elastic.co/v1/versions/$VERSION-SNAPSHOT/builds)
+BUILD_HASH=$(echo $MANIFEST | jq -r '.builds[0]')
+KBN_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/kibana/kibana-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+MBT_DOWNLOAD_URL=https://snapshots.elastic.co/$BUILD_HASH/downloads/beats/metricbeat/metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+
+cat << SCRIPT > /etc/profile.d/custom.sh
+VERSION=$VERSION
+BUILD_HASH=$BUILD_HASH
+KBN_DOWNLOAD_URL=$KBN_DOWNLOAD_URL
+MBT_DOWNLOAD_URL=$MBT_DOWNLOAD_URL
+
+echo
+echo \# Latest build: $BUILD_HASH
+echo \# Manifest updated: $(echo $MANIFEST | jq -r '.manifests["last-update-time"]')
+echo \# Latest download URL: $KBN_DOWNLOAD_URL
+echo \# Installation commands:
+echo
+echo \# Setup Kibana
+echo \    cd \$HOME
+echo \    wget --progress=bar:force:noscroll $KBN_DOWNLOAD_URL
+echo \    tar xzf kibana-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+echo \    cp /vagrant/share/config/kibana.base.yml kibana-$VERSION-SNAPSHOT/config/kibana.yml
+echo \    cp /vagrant/share/config/node.options kibana-$VERSION-SNAPSHOT/config/node.options
+echo \    cp -r /vagrant/share/config/cert-bundle kibana-$VERSION-SNAPSHOT/config/
+echo
+echo \# Set passwords in kibana-$VERSION-SNAPSHOT/config/kibana.yml, then run:
+echo \    cd kibana-$VERSION-SNAPSHOT
+echo \    ./bin/kibana
+echo
+echo \# Setup Metricbeat
+echo \    cd \$HOME
+echo \    wget --progress=bar:force:noscroll $MBT_DOWNLOAD_URL
+echo \    tar xzf metricbeat-$VERSION-SNAPSHOT-linux-x86_64.tar.gz
+echo \    cp /vagrant/share/config/metricbeat.yml metricbeat-$VERSION-SNAPSHOT
+echo \    cd metricbeat-$VERSION-SNAPSHOT
+echo \    ./metricbeat modules enable linux system
+echo \    sudo chown root metricbeat.yml
+echo \    sudo chown root modules.d/system.yml
+echo \    sudo ./metricbeat -e
+echo
+SCRIPT
+
+# Allow access to elasticsearch
+echo "10.0.2.2  elasticsearch" >> /etc/hosts
