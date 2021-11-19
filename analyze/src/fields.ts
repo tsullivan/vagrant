6774aa132a72338c9ecb0a2da546a56b81e3a0cc
Timothy Sullivan <tsullivan@elastic.co>
2021-11-19 16:06:27 -0700

    update for testing with streaming

--- analyze/src/fields.ts
+++ analyze/src/fields.ts
@@ -14,8 +14,13 @@ const datas: DataSet = {
       name: 'start_time',
       type: 'date',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(`Claiming`));
-        if (!line) { return null; }
+        const line = logs
+          .filter(({ log }) => log.logger === 'plugins.reporting.runTask')
+          .find(({ message }) => message.match(`Claiming`));
+
+        if (!line) {
+          return null;
+        }
         const value = line['@timestamp'];
         return moment.utc(value);
       },
@@ -24,8 +29,14 @@ const datas: DataSet = {
       name: 'end_time',
       type: 'date',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(`Chunk size is`));
-        if (!line) { return null; }
+        const line = logs
+          .filter(({ log }) => log.logger === 'plugins.reporting.runTask')
+          .find(({ message }) => message.match(`^Saved`));
+
+        if (!line) {
+          return null;
+        }
+
         const value = line['@timestamp'];
         return moment.utc(value);
       },
@@ -43,10 +54,12 @@ const datas: DataSet = {
     {
       name: 'page_requests',
       getValue(_jobId, logs) {
-        const line = logs.find((str) =>
-          str.message.match(/handled .* page requests/)
+        const line = logs.find(({ message }) =>
+          message.match(/handled .* page requests/)
         );
-        if (!line) { return null; }
+        if (!line) {
+          return null;
+        }
         const value = line.message.replace(/^.*handled (\d+) page requests*$/, '$1');
         return parseInt(value, 10);
       },
@@ -54,8 +67,10 @@ const datas: DataSet = {
     {
       name: 'panels',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(/waiting for \d/));
-        if (!line) { return null; }
+        const line = logs.find((log) => log.message.match(/waiting for \d/));
+        if (!line) {
+          return null;
+        }
         const value = line.message.replace(
           /^.*waiting for (\d+) rendered elements.*$/,
           '$1'
@@ -66,14 +81,18 @@ const datas: DataSet = {
     {
       name: 'render_complete_time',
       getValue(_jobId, logs) {
-        const startLine = logs.find((str) => str.message.match(`Claiming`));
-        if (!startLine) { return null; }
+        const startLine = logs.find((log) => log.message.match(`Claiming`));
+        if (!startLine) {
+          return null;
+        }
         const startTime = moment.utc(startLine['@timestamp']);
 
-        const renderCompleteLine = logs.find((str) =>
-          str.message.match(`rendering is complete`)
+        const renderCompleteLine = logs.find(({ message }) =>
+          message.match(`rendering is complete`)
         );
-        if (!renderCompleteLine) { return null; }
+        if (!renderCompleteLine) {
+          return null;
+        }
         const renderTime = moment.utc(renderCompleteLine['@timestamp']);
 
         return renderTime.valueOf() - startTime.valueOf();
@@ -82,14 +101,18 @@ const datas: DataSet = {
     {
       name: 'take_screenshots_time',
       getValue(_jobId, logs) {
-        const startLine = logs.find((str) => str.message.match(`taking screenshots`));
-        if (!startLine) { return null; }
+        const startLine = logs.find((log) => log.message.match(`(taking|streaming) screenshots`));
+        if (!startLine) {
+          return null;
+        }
         const startTime = moment.utc(startLine['@timestamp']);
 
-        const byteLengthLine = logs.find((str) =>
-          str.message.match(`PNG buffer byte length`)
+        const byteLengthLine = logs.find(({ message }) =>
+          message.match(`PNG buffer byte length`)
         );
-        if (!byteLengthLine) { return null; }
+        if (!byteLengthLine) {
+          return null;
+        }
         const renderTime = moment.utc(byteLengthLine['@timestamp']);
 
         return renderTime.valueOf() - startTime.valueOf();
@@ -98,8 +121,10 @@ const datas: DataSet = {
     {
       name: 'byte_length',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(`buffer byte length`));
-        if (!line) { return null; }
+        const line = logs.find((log) => log.message.match(`buffer byte length`));
+        if (!line) {
+          return null;
+        }
         const value = line.message.replace(/^.*buffer byte length: (\d+).*$/, '$1');
         return parseInt(value, 10);
       },
@@ -107,8 +132,10 @@ const datas: DataSet = {
     {
       name: 'cpu',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(`Chromium consumed CPU`));
-        if (!line) { return null; }
+        const line = logs.find((log) => log.message.match(`Chromium consumed CPU`));
+        if (!line) {
+          return null;
+        }
         const value = line.message.replace(
           /^.*Chromium consumed CPU (\d+)% Memory ([0-9\.]+)MB.*$/,
           '$1'
@@ -119,8 +146,10 @@ const datas: DataSet = {
     {
       name: 'memory',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(`Chromium consumed CPU`));
-        if (!line) { return null; }
+        const line = logs.find((log) => log.message.match(`Chromium consumed CPU`));
+        if (!line) {
+          return null;
+        }
         const string = line.message.replace(/^.*Memory ([\d\.]+)MB/, '$1');
         const valueMbs = parseFloat(string);
         return Math.round(valueMbs * 1000000); // MBs in bytes
@@ -130,14 +159,15 @@ const datas: DataSet = {
   booleans: [
     {
       name: 'used_stream',
-      getValue() {
-        return false; // FIXME
+      getValue(_jobId, logs) {
+        const line = logs.find((log) => log.message.match(`streaming screenshots`));
+        return !!line;
       },
     },
     {
       name: 'completed',
       getValue(_jobId, logs) {
-        const line = logs.find((str) => str.message.match(`Chunk size is`));
+        const line = logs.find((log) => log.message.match(`buffer byte length`));
         return !!line;
       },
     },
